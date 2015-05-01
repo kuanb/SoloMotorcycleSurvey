455733184228ab50d9444f80e277ad807a9c0ab5
buzoherbert <buzoherbert@gmail.com>
2015-05-01 18:37:03 +0800

    randomizing answers

--- Example/script.js
+++ Example/script.js
@@ -1,11 +1,23 @@
-	console.log(JSON.stringify(fSUR.serializeJSON()));
+	// Fisher–Yates shuffle
+	function shuffle(array) {
+		var counter = array.length, temp, index;
+	    // While there are elements in the array
+	    while (counter > 0) {
+	      // Pick a random index
+	      index = Math.floor(Math.random() * counter);
+	      // Decrease counter by 1
+	      counter--;
+	      // And swap the last element with it
+	      temp = array[counter];
+	      array[counter] = array[index];
+	      array[index] = temp;
+	  }
+	  return array;
+	}	
 	var c = fSUR.getChapters()[0];
-	console.log(JSON.stringify(c.serializeJSON()));
 	var q = c.getQuestions()[0];
-	console.log('CHANGING QUESTION');
-	q.setQuestionText(Math.floor(Math.random() * (10 - 2 + 1)) + 2);
-	console.log(JSON.stringify(q.serializeJSON()));
+	q.setQuestionText("Random number:" + Math.floor(Math.random() * (10 - 2 + 1)) + 2);
+	var answers = shuffle(q.getAnswers());
+	q.setAnswers(answers);
 	c.setQuestions(new Array(q));
-	console.log(JSON.stringify(c.serializeJSON()));
 	fSUR.setChapters(new Array(c));
-	console.log(JSON.stringify(fSUR.serializeJSON()));
