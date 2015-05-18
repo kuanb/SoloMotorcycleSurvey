b393258da6d7e3700a2a6a3fc0f25fe28746c357
buzoherbert <buzoherbert@gmail.com>
2015-05-18 20:33:35 +0800

    commented flockScript and better functioning code

--- Example/script.js
+++ Example/script.js
@@ -1,18 +1,23 @@
-	function shuffle(array) {
-		var counter = array.length, temp, index;
-	    while (counter > 0) {
-	      index = Math.floor(Math.random() * counter);
-	      counter--;
-	      temp = array[counter];
-	      array[counter] = array[index];
-	      array[index] = temp;
-	  }
-	  return array;
-	}	
-	var c = fSUR.getChapters()[0];
-	var q = c.getQuestions()[0];
-	q.setQuestionText("Random number: " + Math.floor(Math.random() * (10 - 2 + 1)) + 2);
-	var answers = shuffle(q.getAnswers());
-	q.setAnswers(answers);
-	c.setQuestions(new Array(q));
-	fSUR.setChapters(new Array(c));
+/*
+     This is a test, repeat, this is a test
+*/
+// Function to shuffle the elements of an array
+function shuffle(array) {
+	var counter = array.length, temp, index;
+    while (counter > 0) {
+      index = Math.floor(Math.random() * counter);
+      counter--;
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
+  return array;
+}
+// Getting the appropiate chapter, question and answers and randomizing the question.
+var c = fSUR.getChapters()[0];
+var q = c.getQuestions()[0];
+q.setQuestionText("Hi, I'm a random number: " + (Math.floor(Math.random() * (10 - 2 + 1)) + 2));
+var answers = shuffle(q.getAnswers());
+q.setAnswers(answers);
+c.setQuestions(new Array(q));
+fSUR.setChapters(new Array(c));
